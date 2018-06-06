563f2baee56d596cbffb5290ef62331d52b7f969
csc <csc@LAPTOP-EEVQ1NGM>
2018-06-06 21:32:44 +0900

    Main.java upload

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	/// -> Arguments 탭 -> Working Directory 를 Other : C드라이브퍼StarCraft 로 변경하면<br>
 	/// 봇 프로그램이 기 생성되어있는 맵 파일 분석 캐시파일을 활용하게 되어서<br>
 	/// 봇 프로그램 실행 시 맵 파일 분석에 소요되는 딜레이를 줄일 수 있습니다.
+	// commit test
     public static void main(String[] args) {
     	try{
             new MyBotModule().run();   		
