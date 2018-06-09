4b7a8a928a926ab11eb964eceae2ca55956040f4
csc <csc@LAPTOP-EEVQ1NGM>
2018-06-10 03:36:24 +0900

    config 수정 EnableCompleteMapInformation 사용하지 말것

--- src/Config.java
+++ src/Config.java
@@ -35,7 +35,7 @@ public class Config {
 	/// Fastest: 42 ms/frame.  1초에 24 frame. 일반적으로 1초에 24frame을 기준 게임속도로 합니다<br>
 	/// Normal: 67 ms/frame. 1초에 15 frame<br>
 	/// As fast as possible : 0 ms/frame. CPU가 할수있는 가장 빠른 속도.
-	public static int SetLocalSpeed = 10;
+	public static int SetLocalSpeed = 0;
 	
 	/// 로컬에서 게임을 실행할 때 FrameSkip (코드 제출 후 서버에서 게임을 실행할 때는 서버 설정을 사용함)<br>
 	/// frameskip을 늘리면 화면 표시도 업데이트 안하므로 훨씬 빠릅니다
@@ -44,8 +44,9 @@ public class Config {
     /// 로컬에서 게임을 실행할 때 사용자 키보드/마우스 입력 허용 여부 (코드 제출 후 서버에서 게임을 실행할 때는 서버 설정을 사용함)	
     public static boolean EnableUserInput = true;
     
-    /// 로컬에서 게임을 실행할 때 전체 지도를 다 보이게 할 것인지 여부 (코드 제출 후 서버에서 게임을 실행할 때는 서버 설정을 사용함)    
-	public static boolean EnableCompleteMapInformation = false;
+    /// 로컬에서 게임을 실행할 때 전체 지도를 다 보이게 할 것인지 여부 (코드 제출 후 서버에서 게임을 실행할 때는 서버 설정을 사용함)
+    // 사용하지 말것 sc76.choi : true로 변경 하지 말것(오버로드가 정찰을 못감)
+	//public static boolean EnableCompleteMapInformation = false;
 
 	
 	/// MapGrid 에서 한 개 GridCell 의 size
